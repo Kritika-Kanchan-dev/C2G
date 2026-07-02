cd2bd0dd93eaa828a347a6fc0fa194492394f9ff
Kritika Kanchan <contact.kritikakanchan@gmail.com>
2026-07-02 21:27:12 +0530

    [C2G] Submit: Coding Assignment - Question 1

---
+++ assignments/Coding Assignment/Question 1/question.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Write your solution here
+        int day = sc.nextInt();
+        if(day == 1){
+            System.out.println("Discount available");
+        }else{
+            System.out.println("no Discount");
+        }
+    }
+}
